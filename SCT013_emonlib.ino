656f7cc9d9dbbce59579af109206af4a7d8afdb7
Kamil <kamilsteelS@outlook.com>
2020-04-15 15:28:37 +0200

    Some updates

--- SCT013_emonlib.ino
+++ SCT013_emonlib.ino
@@ -2,6 +2,7 @@
 
 #include "EmonLib.h"                   // Include Emon Library
 EnergyMonitor emon1;                   // Create an instance
+float volt = 228.0;
 
 void setup()
 {  
@@ -13,8 +14,32 @@ void setup()
 void loop()
 {
   double Irms = emon1.calcIrms(1480);  // Calculate Irms only 1480
+
+ if (Irms >= 2.75)
+ {
+   volt = 220.0;
+ } else if (Irms >= 2.9)
+ {
+   volt = 218.0;
+ } else if (Irms >= 4.5)
+ {
+   volt = 217.0;
+ } else if (Irms >= 5.5)
+ {
+   volt = 215.0;
+ } else
+ {
+   volt = 225.0;
+ }
+ 
+ 
+ 
+ 
   
+  Serial.print("Watt: ");
   Serial.print(Irms*218.0);	       // Apparent power
-  Serial.print(" ");
+  Serial.print("   A: ");
   Serial.println(Irms);		       // Irms
+  Serial.print("Volt: ");
+  Serial.print(volt);
 }
