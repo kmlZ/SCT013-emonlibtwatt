ca80f1d46075af1a56446d4ca5f8314831532fcd
Kamil <kamilsteelS@outlook.com>
2020-04-15 17:16:02 +0200

    Work suspended

--- SCT013_emonlib.ino
+++ SCT013_emonlib.ino
@@ -1,36 +1,50 @@
-// EmonLibrary examples openenergymonitor.org, Licence GNU GPL V3
+// Pomiar mocy pozornej
 
-#include "EmonLib.h"                   // Include Emon Library
-EnergyMonitor emon1;                   // Create an instance
+#include "EmonLib.h"                   
 float volt = 228.0;
 
 void setup()
 {  
   Serial.begin(9600);
   
-  emon1.current(1, 60.6);             // Current: input pin, calibration.
+  emon1.current(A1, 55.0);             
 }
 
 void loop()
 {
-  double Irms = emon1.calcIrms(1480);  // Calculate Irms only 1480
+  double Irms = emon1.calcIrms(1480);
 
  if (Irms >= 2.75)
  {
    volt = 220.0;
- } else if (Irms >= 2.9)
+ }
+
+ if (Irms >= 2.9)
  {
    volt = 218.0;
- } else if (Irms >= 4.5)
+ }
+
+ if (Irms >= 4.5)
  {
    volt = 217.0;
- } else if (Irms >= 5.5)
+ }
+
+ if (Irms >= 5.5)
  {
    volt = 215.0;
- } else
- {
-   volt = 225.0;
  }
+
+ if (Irms >= 7.5)
+ {
+   volt = 209;
+ }
+
+ if (Irms > 10)
+ {
+   volt = 207;
+ }
+ 
+ 
  
  
  
